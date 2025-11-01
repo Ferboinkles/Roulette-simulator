5f266a8e295f1ce1129028df89ed2d7d8fff8411
Ferboinkles <vihaan4nasa@gmail.com>
2025-11-01 20:03:33 +0000

    Added spaces around operators :0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
+<script>
+</script>
+
 <div
-	class="@container flex w-full flex-col justify-center text-center font-serif"
+	class=" @container flex w-full flex-col justify-center text-center font-serif"
 >
 	<h1
-		class="text-center font-serif text-6xl text-blue-800"
+		class=" text-center font-serif text-6xl text-blue-800"
 	>
 		Welcome to the Roulette Simulator!
 	</h1>
-	<div class="@container text-center font-serif">
+	<div class=" @container text-center font-serif">
 		<button
 			class=" [animation-duration:0.5s] hover:animate-spin"
 			><img
-				alt="wheel"
-				src="/images/wheel.webp"
+				alt=" wheel "
+				src=" /images/wheel.webp "
 			/>
 		</button>
 	</div>
