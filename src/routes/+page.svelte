79c95828d29cfbd3d1730da337ea09acef60d4e6
Ferboinkles <vihaan4nasa@gmail.com>
2025-11-02 20:44:06 +0000

    Added buttons with colours at the top right

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,58 @@
 <script>
+	let bets = $state(0);
 </script>
 
-<div
-	class=" @container flex w-full flex-col justify-center text-center font-serif"
+<h1
+	class="text-center font-serif text-4xl text-blue-800"
 >
-	<h1
-		class=" text-center font-serif text-6xl text-blue-800"
+	Welcome to the Roulette Simulator!
+</h1>
+<div
+	class="@container flex-row text-center font-serif"
+>
+	<button class=" fixed hover:animate-spin"
+		><img alt="wheel" src="/images/wheel.webp" />
+	</button>
+	<div
+		class="@container flex-row text-right font-serif"
 	>
-		Welcome to the Roulette Simulator!
-	</h1>
-	<div class=" @container text-center font-serif">
 		<button
-			class=" [animation-duration:0.5s] hover:animate-spin"
-			><img
-				alt=" wheel "
-				src=" /images/wheel.webp "
-			/>
+			class="my-1 max-w-fit rounded-[0.5em] bg-black font-serif whitespace-normal text-white hover:scale-110 hover:animate-bounce"
+		>
+			Black
+		</button>
+		<button
+			class="my-1 max-w-fit rounded-[0.5em] bg-red-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+		>
+			Red
+		</button>
+	</div>
+	<div
+		class="@container flex-row text-right font-serif"
+	>
+		<button
+			class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+		>
+			Even
+		</button>
+		<button
+			class="my-1 max-w-fit rounded-[0.5em] bg-black font-serif whitespace-normal text-white hover:scale-110 hover:animate-bounce"
+		>
+			Odd
+		</button>
+	</div>
+	<div
+		class="@container flex-row text-right font-serif"
+	>
+		<button
+			class=" my-1 max-w-fit rounded-[0.5em] bg-red-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+		>
+			High
+		</button>
+		<button
+			class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+		>
+			Low
 		</button>
 	</div>
 </div>
