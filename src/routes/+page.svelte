190a1a2ea0cca988ed4007e912fb77e6308924d2
Ferboinkles <vihaan4nasa@gmail.com>
2025-11-03 18:32:05 +0000

    Added explanation to Low button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,7 +49,7 @@
 		<button
 			class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 		>
-			Low
+			Low (1-18)
 		</button>
 		<div
 			class="@container flex-row text-right font-serif"
