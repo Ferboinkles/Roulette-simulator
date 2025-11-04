5560ed2343761766b1cfd8bb81b313872458dc79
Ferboinkles <vihaan4nasa@gmail.com>
2025-11-04 19:21:23 +0000

    Added green button and changed the colour of all green bootons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,16 @@
 	<button class="w-[35%] text-center hover:animate-spin"
 		><img alt="wheel" src="/images/wheel.webp" class="mx-auto my-4 aspect-[1/1] w-full object-cover" />
 	</button>
-	<div class="@container mr-15 flex-row text-right font-serif">
+	<div class="@container mr-15 flex-row text-center font-serif">
 		<button
 			class="my-1 max-w-fit rounded-[0.5em] bg-black font-serif whitespace-normal text-white hover:scale-110 hover:animate-bounce"
 		>
-			Black (2, 4, 6, 8, 10, 11, 13, 15, 17, 20,22, 24, 26, 28, 29, 31, 33 and 35)
+			Black (2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33 and 35)
+		</button>
+		<button
+			class="my-1 max-w-fit rounded-[0.5em] bg-green-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+		>
+			Green (0)
 		</button>
 		<button
 			class="my-1 max-w-fit rounded-[0.5em] bg-red-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
@@ -15,9 +20,9 @@
 			Red (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, and 36)
 		</button>
 	</div>
-	<div class="@container mr-15 flex-row text-right font-serif">
+	<div class="@container mr-15 flex-row text-center font-serif">
 		<button
-			class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+			class=" my-1 max-w-fit rounded-[0.5em] bg-green-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 		>
 			Even (Even numbers between 1-36)
 		</button>
@@ -27,20 +32,20 @@
 			Odd (Odd numbers between 1-36)
 		</button>
 	</div>
-	<div class="@container mr-15 flex-row text-right font-serif">
+	<div class="@container mr-15 flex-row text-center font-serif">
 		<button
 			class=" my-1 max-w-fit rounded-[0.5em] bg-red-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 		>
 			High (19-36)
 		</button>
 		<button
-			class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+			class=" my-1 max-w-fit rounded-[0.5em] bg-green-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 		>
 			Low (1-18)
 		</button>
-		<div class="@container flex-row text-right font-serif">
+		<div class="@container flex-row text-center font-serif">
 			<button
-				class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
+				class=" my-1 max-w-fit rounded-[0.5em] bg-green-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 			>
 				Dozens (1-12,12-24 or 24-36)
 			</button>
