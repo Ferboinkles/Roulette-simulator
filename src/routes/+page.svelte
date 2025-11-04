4bd4db8ae81c27e8cf22579f1d43f847037e56ae
Ferboinkles <vihaan4nasa@gmail.com>
2025-11-04 19:12:59 +0000

    Changed prettier and fixed the wheel size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,21 @@
-<h1
-	class="text-center font-serif text-4xl text-blue-800"
->
-	Welcome to the Roulette Simulator!
-</h1>
-<div
-	class="@container min-h-full flex-row text-center font-serif"
->
-	<button
-		class="max-w-fit flex-col text-center hover:animate-spin"
-		><img alt="wheel" src="/images/wheel.webp" />
+<h1 class="text-center font-serif text-4xl text-blue-800">Welcome to the Roulette Simulator!</h1>
+<div class="@container min-h-full flex-row text-center font-serif">
+	<button class="w-[35%] text-center hover:animate-spin"
+		><img alt="wheel" src="/images/wheel.webp" class="mx-auto my-4 aspect-[1/1] w-full object-cover" />
 	</button>
-	<div
-		class="@container mr-15 flex-row text-right font-serif"
-	>
+	<div class="@container mr-15 flex-row text-right font-serif">
 		<button
 			class="my-1 max-w-fit rounded-[0.5em] bg-black font-serif whitespace-normal text-white hover:scale-110 hover:animate-bounce"
 		>
-			Black (2, 4, 6, 8, 10, 11, 13, 15, 17,
-			20,22, 24, 26, 28, 29, 31, 33 and 35)
+			Black (2, 4, 6, 8, 10, 11, 13, 15, 17, 20,22, 24, 26, 28, 29, 31, 33 and 35)
 		</button>
 		<button
 			class="my-1 max-w-fit rounded-[0.5em] bg-red-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 		>
-			Red (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21,
-			23, 25, 27, 30, 32, 34, and 36)
+			Red (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, and 36)
 		</button>
 	</div>
-	<div
-		class="@container mr-15 flex-row text-right font-serif"
-	>
+	<div class="@container mr-15 flex-row text-right font-serif">
 		<button
 			class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 		>
@@ -40,9 +27,7 @@
 			Odd (Odd numbers between 1-36)
 		</button>
 	</div>
-	<div
-		class="@container mr-15 flex-row text-right font-serif"
-	>
+	<div class="@container mr-15 flex-row text-right font-serif">
 		<button
 			class=" my-1 max-w-fit rounded-[0.5em] bg-red-700 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 		>
@@ -53,9 +38,7 @@
 		>
 			Low (1-18)
 		</button>
-		<div
-			class="@container flex-row text-right font-serif"
-		>
+		<div class="@container flex-row text-right font-serif">
 			<button
 				class=" my-1 max-w-fit rounded-[0.5em] bg-green-800 font-serif whitespace-normal hover:scale-110 hover:animate-bounce"
 			>
