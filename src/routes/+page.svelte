e452a71a89619591c815bb84a48d83f6b6b9eb25
Ferboinkles <vihaan4nasa@gmail.com>
2025-11-07 17:41:29 +0000

    Added money bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import { goto } from "$app/navigation";
+	let money = $state(1000000);
 </script>
 
+<p class="max-w-fit rounded-full bg-white p-3 text-cyan-600">
+	You have ${money} left!
+</p>
 <h1 class="text-center font-serif text-5xl text-cyan-600">Welcome to the Roulette Simulator!</h1>
 <h2 class="text-center font-serif text-2xl text-cyan-600">Click on a button to see the probability of your bet!</h2>
 <div class="@container min-h-full flex-row text-center font-serif">
@@ -21,7 +25,7 @@
 		</button>
 		<button
 			class="my-1 max-w-fit rounded-[0.5em] bg-red-700 p-[0.5em] font-serif whitespace-normal hover:animate-bounce"
-			onclick={goto("/Red-button")}
+			onclick={goto("/red-button")}
 		>
 			Red (1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, and 36)
 		</button>
